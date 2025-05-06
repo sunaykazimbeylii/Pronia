f465959a79d9119864a245d4b81644d6c83bc708
kazimbeyli <sunaykazimbeyli@gmail.com>
2025-05-06 10:37:10 +0400

    Small fixes in Slide CRUD

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,38 +15,10 @@ namespace Pronia.Controllers
         }
         public async Task <IActionResult> Index()
         {
-            List<Slide> slides = new List<Slide>
-            {
-                new Slide
-            {
-                Title="NEW PLANT",
-                SubTitle="65% OFF",
-                Description="Pronia, With 100% Natural, Organic &amp; Plant Shop.",
-                Order=2,
-                Image="1-1-524x617.png",
-                CreatedAt=DateTime.Now
-            },
-                   new Slide
-            {
-                Title="NEW PLANT",
-                SubTitle="65% OFF",
-                Description="Pronia, With 100% Natural, Organic &amp; Plant Shop.",
-                Order=3,
-                Image="1-2-524x617.png",
-                CreatedAt=DateTime.Now
-            },
-                      new Slide
-            {
-                Title="NEW PLANT",
-                SubTitle="65% OFF",
-                Description="Pronia, With 100% Natural, Organic &amp; Plant Shop.",
-                Order=1,
-                Image="1-3-270x300.jpg",
-                CreatedAt=DateTime.Now
-            }
-            };
-            _context.Slides.AddRange(slides);
-            _context.SaveChanges();
+            List<Slide> slides = await _context.Slides.ToListAsync();
+            
+            //_context.Slides.AddRange(slides);
+            //_context.SaveChanges();
             HomeVM homeVM= new HomeVM
             {
                 Slides=await _context.Slides
--- obj/Debug/net8.0/Pronia.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a35658403840538fc72a933ad272c7ab62c212")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0564caf8944e076bfefe13a24b4d1eb37b98414c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
